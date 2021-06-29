d62f8be6512a2f8bc8b8be75b65fb5ba66bc2b43
sae-yajima <y.sae1338@icloud.com>
2021-06-29 21:54:58 +0900

    検索機能変更(未)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
+
     def show
         @user = User.find(params[:id])
         @name = @user.name
         @pets = @user.pets
-
     end
-
-    def search
-        @users = User.search(params[:keyword])
-     end
+     
+    
     
 end
