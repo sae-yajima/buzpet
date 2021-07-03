b7376179bca20b0dbc7093b515530285d7de998c
sae-yajima <y.sae1338@icloud.com>
2021-07-03 20:39:20 +0900

    いいね機能　実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ root to: "pets#index"
   devise_for :users
   resources :pets do
     resources :comments, only: :create
-    collection do
+     collection do
       get 'search'
-    end
+     end
   end
+   resources :posts do
+    post 'add' => 'likes#create'
+    delete '/add' => 'likes#destroy'
+   end
   resources  :users,only: :show
 end
